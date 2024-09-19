941ff8cad3fbb6c7a07688ecb9d17e90b5fd9819
Acer <gugunmfauzi16@gmail.com>
2024-09-19 11:12:49 +0700

    singkatan variavel

--- Dasar_Sintaks_Pengulangan_dengan while_undefined_count.py
+++ Dasar_Sintaks_Pengulangan_dengan while_undefined_count.py
@@ -1,25 +1,25 @@
 """
 Program pengulangan membaca buku undefined count
 """
-jumlah_buku = 10
+buku = 10
 print('Ibu berkata, "Baca semua buku"')
-buku = 0
+jumlah_baca = 0
 
-jumlah_buku_yang_sudah_dibaca_dan_dipahami = 0
-print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
+jumlah_paham = 0
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_paham}')
 
-while buku < jumlah_buku * 2:
-    buku = buku + 1
-    if jumlah_buku_yang_sudah_dibaca_dan_dipahami == 9:
-        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} belum paham")
+while jumlah_baca < buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_paham == 9:
+        print(f"Buku ke {jumlah_paham} belum paham")
     else :
-        jumlah_buku_yang_sudah_dibaca_dan_dipahami = jumlah_buku_yang_sudah_dibaca_dan_dipahami + 1
-        print(f"Buku ke {jumlah_buku_yang_sudah_dibaca_dan_dipahami} sudah dibaca dan dipahami")
+        jumlah_paham = jumlah_paham + 1
+        print(f"Buku ke {jumlah_paham} sudah dibaca dan dipahami")
 
-print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami}')
-if jumlah_buku_yang_sudah_dibaca_dan_dipahami == jumlah_buku:
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_paham}')
+if jumlah_paham == buku:
     print('"Bu, semua buku sudah dibaca dan dipahami')
 else :
     print(f'"Bu, tidak semua buku bisa dipahami. '
-          f'Budi hanya bisa memahami {jumlah_buku_yang_sudah_dibaca_dan_dipahami} buku')
+          f'Budi hanya bisa memahami {jumlah_paham} buku')
 
